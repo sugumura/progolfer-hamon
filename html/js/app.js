f4ee7ff0fd69ca9765d8f6182a8aef1342e96393
Suguru Murakami <sugumura@gmail.com>
2017-01-21 02:58:52 +0900

    Add use createjs

--- html/js/app.js
+++ html/js/app.js
@@ -1,75 +1,68 @@
 'use strict';
+
 document.addEventListener("DOMContentLoaded", init);
 
-var rocketAsset = 'assets/images/rocket.png';
+var stage;
+var queue = new createjs.LoadQueue();
+queue.installPlugin(createjs.Sound);
+queue.on("complete", handleComplete, this);
 
-// ref: http://yomotsu.net/blog/2013/01/05/fps.html
-var now = window.performance && (
-    performance.now ||
-    performance.mozNow ||
-    performance.msNow ||
-    performance.oNow ||
-    performance.webkitNow );
+queue.loadManifest([
+    {id: "rocket", src: 'assets/images/rocket.png'},
+    {id: "star", src: 'assets/images/star.png'}
+]);
 
-var getTime = function() {
-    return ( now && now.call( performance ) ) || ( new Date().getTime() );
+var background = {};
+background.entire = function () {
+    var background = new createjs.Shape();
+    background.graphics.beginFill("DeepSkyBlue").drawRect(0, 0, 768, 768);
+    background.x = 0;
+    background.y = 0;
+    return background;
+};
+background.sidebar = function () {
+    var side = new createjs.Shape();
+    side.graphics.beginFill("Grey").drawRect(518, 0, 250, 768);
+    side.x = 0;
+    side.y = 0;
+    return side;
 };
 
 
 function init(event) {
+
     console.log('DOMContentLoaded', event);
+    stage = new createjs.Stage("GameWindow");
 
-    //Create the renderer
-    var renderer = PIXI.autoDetectRenderer(768, 768, {backgroundColor: 0x1099bb});
-
-    //Add the canvas to the HTML document
-    var GameWindow = document.getElementById('GameWindow');
-    GameWindow.appendChild(renderer.view);
-
-    //Create a container object called the `stage`
-    var stage = new PIXI.Container();
-
-
-    PIXI.loader
-        .add(rocketAsset)
-        .load(setup);
-
-    function setup() {
-        var rocket = new PIXI.Sprite(PIXI.loader.resources[rocketAsset].texture);
-        stage.addChild(rocket);
-
-    }
-
-    // サイドバー
-    var sidebar = new PIXI.Graphics();
-    sidebar.beginFill(0xf0f8ff);
-    sidebar.drawRect(518, 0, 250, 768);
+    var entire = background.entire();
+    var sidebar = background.sidebar();
+    stage.addChild(entire);
     stage.addChild(sidebar);
 
-    /**
-     * アニメーション
-     */
-    var startTime = getTime();
-    var fps = 30;
-    var frameLength = 30;
-    var preFrame = 0;
+    createjs.Ticker.setFPS(30);
+    createjs.Ticker.addEventListener('tick', function(e){
+        // console.log(e);
+        stage.update();
+    });
+}
 
-    function animate(timestamp) {
-        window.requestAnimationFrame(animate);
-        renderer.render(stage);
-        PIXI.actionManager.update(); // update actions
+function handleComplete(event) {
+    console.log(event);
+    var rocket = new createjs.Bitmap(queue.getResult('rocket'));
+    rocket = anchorCenter(rocket);
+    rocket.x = 259;
+    rocket.y = 668;
+    stage.addChild(rocket);
 
-        var lastTime = getTime();
-        var frame = Math.floor( ( lastTime - startTime ) / ( 1000.0 / fps ) % frameLength );
-        if (preFrame < frame) {
-            if (frame === frameLength - 1) {
-                preFrame = 0;
-            } else {
-                preFrame = frame;
-            }
-        }
-    }
+    var star = new createjs.Bitmap(queue.getResult('star'));
+    star = anchorCenter(star);
+    star.x = 259;
+    star.y = 100;
+    stage.addChild(star);
+}
 
-    animate();
-
+function anchorCenter(item) {
+    item.regX = item.getBounds().width / 2;
+    item.regY = item.getBounds().height / 2;
+    return item;
 }
