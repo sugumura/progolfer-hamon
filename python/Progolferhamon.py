4ce208401bb504d69157bf0e231960f063dc9027
eolla1013 <eolla1013@gmail.com>
2017-01-21 17:05:34 +0900

    サーバ側のマージ及びリクエスト受付部分追加

--- python/Progolferhamon.py
+++ python/Progolferhamon.py
@@ -3,24 +3,34 @@ import PhysicalCalculation as py
 class ProgolferHamon:
     def __init__(self):
         self.__StarShip=None
-        self.__Stars=None
+        self.__Stars=list()
         self.__StageInfo=None
-        self.__Frames=None
+        self.__Frames=list()
+
     def set_StarShip(self,ship):
         self.__StarShip=ship
     def get_StarShip(self):
         return self.__StarShip
     StarShip=property(get_StarShip,set_StarShip)
+
     def set_Stars(self,starlst):
         self.__Stars=starlst
     def get_Stars(self):
         return self.__Stars
-    StarShip=property(set_Stars,get_Stars)
+    Stars=property(get_Stars,set_Stars)
+
     def set_StageInfo(self,stage):
         self.__StageInfo=stage
     def get_StageInfo(self):
         return self.__StageInfo
-    StarShip=property(set_StageInfo,get_StageInfo)
+    StageInfo=property(get_StageInfo,set_StageInfo)
+
+    def set_Frames(self,fmlst):
+        self.__Frames=fmlst
+    def get_Frames(self):
+        return self.__Frames
+    Frames=property(get_Frames,set_Frames)
+
 
     def createPositionData(self):
         self.Frames=None
@@ -37,6 +47,11 @@ class StarShip:
         self.y = y
         self.speed = sp
         self.drection = dr
+
+    def getJson(self):
+        #return '{"x":"{0}","y":"{1}","speed":"{1}","direction":"{1}"}'.format(self.x,self.y,self.speed,self.direction)
+        ret='"x":"{0}","y":"{1}","speed":"{2}","direction":"{3}"'.format(self.x,self.y,self.speed,self.direction)
+        return '{' + ret + '}'
 
 class Star:
     def __init__(self,x,y,g):
--- python/application.py
+++ python/application.py
@@ -1,5 +1,11 @@
+# coding: utf-8
+
+import cgi
+import json
 import logging
 import logging.handlers
+import sys
+import Progolferhamon
 
 from wsgiref.simple_server import make_server
 
@@ -9,7 +15,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Handler 
-LOG_FILE = '/opt/python/log/sample-app.log'
+LOG_FILE = '/opt/python/log/hamonweb-app.log'
+#LOG_FILE = './hamonweb-app.log'
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
 handler.setLevel(logging.INFO)
 
@@ -40,6 +47,7 @@ def application(environ, start_response):
     path    = environ['PATH_INFO']
     method  = environ['REQUEST_METHOD']
     if method == 'POST':
+        response = ''
         try:
             if path == '/':
                 request_body_size = int(environ['CONTENT_LENGTH'])
@@ -47,21 +55,84 @@ def application(environ, start_response):
                 logger.info("Received message: %s" % request_body)
             elif path == '/scheduled':
                 logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'], environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
-            elif path == '/rungame':
-                logger.info("Running Game!!")
+            elif path == '/startgame':
+                logger.info("Start Game!!")
+                
+                #wsgi_input = environ['wsgi.input']
+                #length = int(environ.get('CONTENT_LENGTH', 0))
+                #req = dict(cgi.parse_qsl(wsgi_input.read(length).decode()))
+                #for arg in req:
+                #    logger.info('key:' + arg + ",value:" + req[arg])
+
+                #logger.info("--Request Data 1")
+
+                #reqstr=(req[' name']).replace('request','')
+                #logger.info(reqstr)
+                post_env = environ.copy()
+                post_env['QUERY_STRING'] = ''
+                post = cgi.FieldStorage(fp=environ['wsgi.input'],environ=post_env,keep_blank_values=True)
+                reqstr=post['request'].value
+                logger.info(reqstr)
+
+                response=startgameservice(reqstr)
+                logger.info("--Response Data 1")
+                logger.info(response)
+
         except (TypeError, ValueError):
             logger.warning('Error retrieving request body for async work.')
-        response = ''
+            logger.warning(sys.exc_info())
+        status = '200 OK'
+        headers = [('Content-type', 'application/json')]
     else:
         response = welcome
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
+        status = '200 OK'
+        headers = [('Content-type', 'text/html')]
 
     start_response(status, headers)
     return [response]
 
+def startgameservice(reqstr):
+    logger.info("--Parse JSON")
+    reqobj=json.loads(reqstr)
+
+    logger.info("--Create Model")
+    model=Progolferhamon.ProgolferHamon()
+
+    model.StarShip=Progolferhamon.StarShip()
+    model.StarShip.x=reqobj["starship"]["x"]
+    model.StarShip.y=reqobj["starship"]["y"]
+    model.StarShip.speed=reqobj["starship"]["speed"]
+    model.StarShip.direction=reqobj["starship"]["direction"]
+
+    model.StageInfo=Progolferhamon.StageInfo()
+    model.StageInfo.id=reqobj["stageinfo"]["id"]
+    model.StageInfo.name=reqobj["stageinfo"]["name"]
+
+    for itm in reqobj["stars"]:
+        star=Progolferhamon.Star()
+        star.x=itm["x"]
+        star.y=itm["y"]
+        star.glavity=itm["glavity"]
+        model.Stars.append(star)
+
+    logger.info("--Create Position")
+    model.createPositionData()
+
+    logger.info("--Output Result")
+    resstr=""
+    for itm in model.Frames:
+        if(resstr=="") :
+            resstr='{ "frames" : ['
+        else:
+            resstr=resstr + "],["
+        resstr=resstr + itm.getJson()
+
+    resstr=resstr + "]}"
+
+    return resstr
+
 
 if __name__ == '__main__':
-    httpd = make_server('', 8000, application)
-    print("Serving on port 8000...")
+    httpd = make_server('', 8080, application)
+    print("Serving on port 8080...")
     httpd.serve_forever()
