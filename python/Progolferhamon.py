0310bd256ccf4358f2d90a5fef96549fa62eb179
eolla1013 <eolla1013@gmail.com>
2017-01-21 12:09:38 +0900

    追加プロパティおよび実行のダミーメソッド追加

--- python/Progolferhamon.py
+++ python/Progolferhamon.py
@@ -2,15 +2,28 @@
 
 class ProgolferHamon:
     def __init__(self):
-        self.__StarShip=""
-        self.__Stars=""
-        self.__StageInfo=""
-
+        self.__StarShip=None
+        self.__Stars=None
+        self.__StageInfo=None
+        self.__Frames=None
     def set_StarShip(self,ship):
         self.__StarShip=ship
     def get_StarShip(self):
         return self.__StarShip
     StarShip=property(get_StarShip,set_StarShip)
+    def set_Stars(self,starlst):
+        self.__Stars=starlst
+    def get_Stars(self):
+        return self.__Stars
+    StarShip=property(set_Stars,get_Stars)
+    def set_StageInfo(self,stage):
+        self.__StageInfo=stage
+    def get_StageInfo(self):
+        return self.__StageInfo
+    StarShip=property(set_StageInfo,get_StageInfo)
+
+    def createPositionData(self):
+        self.Frames=None
 
 class StarShip:
     def __init__(self):
